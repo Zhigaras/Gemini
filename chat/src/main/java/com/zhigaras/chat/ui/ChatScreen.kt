31d9d18e23c8676ec456bd0a8af901c6ec9dacd7
Ilya Zhigaras <zhigaras.dev@gmail.com>
2024-01-02 22:53:46 +0600

    send button refactored

--- chat/src/main/java/com/zhigaras/chat/ui/ChatScreen.kt
+++ chat/src/main/java/com/zhigaras/chat/ui/ChatScreen.kt
@@ -4,13 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -19,9 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.zhigaras.chat.R
@@ -42,30 +35,23 @@ fun ChatScreen(viewModel: ChatViewModel = koinViewModel()) {
                 it.Show()
             }
         }
-        Row(
-            modifier = Modifier
-                .wrapContentHeight(Alignment.CenterVertically)
-                .height(IntrinsicSize.Max)
-        ) {
-            TextField(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f),
-                value = state.inputText,
-                onValueChange = { viewModel.handleAction(ChatUiAction.PromptInputChanged(it)) },
-                placeholder = {
-                    Text(text = stringResource(id = R.string.prompt_placeholder))
-                },
-            )
-            Image(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(10.dp)
-                    .clickable { viewModel.send() },
-                contentScale = ContentScale.FillHeight,
-                imageVector = Icons.Filled.Send,
-                contentDescription = stringResource(id = R.string.send)
-            )
-        }
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = state.inputText,
+            onValueChange = { viewModel.handleAction(ChatUiAction.PromptInputChanged(it)) },
+            placeholder = {
+                Text(text = stringResource(id = R.string.prompt_placeholder))
+            },
+            trailingIcon = {
+                if (state.inputText.isNotBlank())
+                    Image(
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clickable { viewModel.send() },
+                        imageVector = Icons.Filled.Send,
+                        contentDescription = stringResource(id = R.string.send)
+                    )
+            }
+        )
     }
 }
